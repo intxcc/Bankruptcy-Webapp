444571f98b0f110c158f7c067a12ccbb38e0048c
intx <dev@intx.cc>
2018-03-17 10:04:59 +0100

    Added Home Header.

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,12 +2,19 @@
 
 import React from 'react'
 
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Start = props => (
   <div id="home_wrapper">
     <header id="home_header">
-
+      <div id="home_logo_wrapper">
+        <div id="home_logo_logo">
+          B<span>ankruptcy</span>!
+        </div>
+        <div id="home_logo_caption">
+          No, really. You are a good daytrader.
+        </div>
+      </div>
     </header>
     <main id="home_content">
     </main>
