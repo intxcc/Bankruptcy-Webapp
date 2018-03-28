4028ee9ebef76955278610b1a0c52aea46882b71
intx <dev@intx.cc>
2018-03-29 01:31:39 +0200

    Tidied up a little. Home link now not to #top anymore due to display glitches (TODO) Changed the design to look more uniform black/white with strong cubic forms.

--- src/Main.jsx
+++ src/Main.jsx
@@ -94,7 +94,7 @@ class Main extends Component {
       <div className={headerClassName} id="main_wrapper">
         <a id="top" name="top"></a>
         <header id="main_header" ref={mainHeader => { this.mainHeader = mainHeader }}>
-          <HashLink smooth id="main_logo_wrapper_link" to="/dashboard/#top">
+          <HashLink smooth id="main_logo_wrapper_link" to="/dashboard/">
             <div id="main_logo_wrapper" ref={mainHeaderLogo => { this.mainHeaderLogo = mainHeaderLogo }}>
               <div id="main_logo_logo">
                 B<span>ankruptcy</span>!
@@ -108,7 +108,7 @@ class Main extends Component {
         <div id="permanent_header_wrapper_background">
         </div>
         <div id="permanent_header_wrapper">
-          <HashLink smooth to="/dashboard/#top">
+          <HashLink smooth to="/dashboard/">
             <div id="permanent_logo">
               B<span>ankruptcy</span>!
             </div>
