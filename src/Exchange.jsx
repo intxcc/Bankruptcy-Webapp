54be9acb27ac43d12c46fa4d799ef23b85cbd85f
intx <dev@intx.cc>
2018-03-28 06:05:32 +0200

    Added search form to exchange. Added gray border to side menu, as well as permanent header.

--- src/Exchange.jsx
+++ src/Exchange.jsx
@@ -4,7 +4,15 @@ import React from 'react'
 
 const Exchange = (props) => (
   <div id="exchange_wrapper">
-    LOGKLOPGJUIHGUIWE
+    <div id="exchange-search-input-wrapper">
+      <span id="exchange-search-input-label">
+        Search
+      </span>
+      <div id="exchange-search-input">
+        <input placeholder="e.g. Ethereum" id="exchange-search-input-text" type="text" />
+        <i id="exchange-search-input-button" className="fas fa-search"></i>
+      </div>
+    </div>
   </div>
 )
 
